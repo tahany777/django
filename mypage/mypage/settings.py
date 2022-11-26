aad80abc448865b0b57e99ab74f9f5b4c7e1c13e
tahany777 <tahanymohammed777@hotmail.com>
2022-11-26 22:17:13 +0200

    style

--- mypage/mypage/settings.py
+++ mypage/mypage/settings.py
@@ -129,8 +129,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]  
-STATIC_ROOT = BASE_DIR / "staticfiles" 
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"  
+  
